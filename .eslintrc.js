c8664e5636d3ea3378b86d0eac4bd5e0f5d85818
Marko Ristin <marko@ristin.ch>
2021-02-25 23:23:37 +0100

    Kick off (#1)
    This is the initial version of the Swagger UI extension.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false
+    },
+    ecmaVersion: 12
+  },
+  plugins: [
+  ],
+  rules: {
+  }
+}
